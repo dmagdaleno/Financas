55bdeb6532a81ba95f0373ff4fdc76c2a02a7fe2
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-06-30 18:57:00 -0300

    implementar getCount

--- app/src/main/java/br/com/dmagdaleno/financas/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/br/com/dmagdaleno/financas/adapter/ListaTransacoesAdapter.kt
@@ -26,6 +26,6 @@ class ListaTransacoesAdapter(context: Context,
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return transacoes.size
     }
 }
