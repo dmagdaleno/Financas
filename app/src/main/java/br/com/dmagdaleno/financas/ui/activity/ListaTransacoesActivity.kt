99cdd3a820aab69bb157671d566e0a07efc23e9b
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-06-30 18:57:27 -0300

    utilizar novo adapter criado na lista

--- app/src/main/java/br/com/dmagdaleno/financas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/dmagdaleno/financas/ui/activity/ListaTransacoesActivity.kt
@@ -2,8 +2,8 @@ package br.com.dmagdaleno.financas.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
 import br.com.dmagdaleno.financas.R
+import br.com.dmagdaleno.financas.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity: AppCompatActivity() {
@@ -13,8 +13,7 @@ class ListaTransacoesActivity: AppCompatActivity() {
         setContentView(R.layout.activity_lista_transacoes)
 
         val transacoes: List<String> = listOf("Gasto - R$100", "Lucro - R$1025,10")
-        val adapter = ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, transacoes)
+        val adapter = ListaTransacoesAdapter(this, transacoes)
 
         lista_transacoes_listview.adapter = adapter
     }
