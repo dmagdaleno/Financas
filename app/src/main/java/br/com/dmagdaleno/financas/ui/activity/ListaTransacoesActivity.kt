8e0a3f0d0eb5f3c2446e947717253373a2de9c29
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-01 15:10:38 -0300

    mudança do tipo de transacao de String para Transacao

--- app/src/main/java/br/com/dmagdaleno/financas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/dmagdaleno/financas/ui/activity/ListaTransacoesActivity.kt
@@ -3,8 +3,11 @@ package br.com.dmagdaleno.financas.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.dmagdaleno.financas.R
+import br.com.dmagdaleno.financas.model.Transacao
 import br.com.dmagdaleno.financas.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import java.math.BigDecimal
+import java.util.*
 
 class ListaTransacoesActivity: AppCompatActivity() {
 
@@ -12,7 +15,10 @@ class ListaTransacoesActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes: List<String> = listOf("Gasto - R$100", "Lucro - R$1025,10")
+        val transacoes: List<Transacao> = listOf(
+                Transacao(BigDecimal(20.5),  "gasto", Calendar.getInstance()),
+                Transacao(BigDecimal(100),  "receita", Calendar.getInstance()))
+
         val adapter = ListaTransacoesAdapter(this, transacoes)
 
         lista_transacoes_listview.adapter = adapter
