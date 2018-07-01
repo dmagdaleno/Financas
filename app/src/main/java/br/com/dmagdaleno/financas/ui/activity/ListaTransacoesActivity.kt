733ffab1a58720ebf4b574f972101dc60432a8f7
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-01 19:06:19 -0300

    Refatorando a activity e o adapter

--- app/src/main/java/br/com/dmagdaleno/financas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/dmagdaleno/financas/ui/activity/ListaTransacoesActivity.kt
@@ -3,11 +3,11 @@ package br.com.dmagdaleno.financas.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.dmagdaleno.financas.R
+import br.com.dmagdaleno.financas.model.Tipo
 import br.com.dmagdaleno.financas.model.Transacao
 import br.com.dmagdaleno.financas.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import java.math.BigDecimal
-import java.util.*
 
 class ListaTransacoesActivity: AppCompatActivity() {
 
@@ -15,12 +15,44 @@ class ListaTransacoesActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes: List<Transacao> = listOf(
-                Transacao(BigDecimal(20.5),  "gasto", Calendar.getInstance()),
-                Transacao(BigDecimal(100),  "receita", Calendar.getInstance()))
+        val transacoes: List<Transacao> = listaTransacoesDeExemplo()
 
+        configuraListaTransacoes(transacoes)
+    }
+
+    private fun configuraListaTransacoes(transacoes: List<Transacao>) {
         val adapter = ListaTransacoesAdapter(this, transacoes)
 
         lista_transacoes_listview.adapter = adapter
+    }
+
+    private fun listaTransacoesDeExemplo(): List<Transacao> {
+        return listOf (
+                Transacao(
+                    valor = BigDecimal(10),
+                    categoria = "Despesa indefinida",
+                    tipo = Tipo.DESPESA
+                ),
+                Transacao(
+                    valor = BigDecimal(1000),
+                    categoria = "Lazer",
+                    tipo = Tipo.DESPESA
+                ),
+                Transacao(
+                    valor = BigDecimal(9000),
+                    categoria = "Salário",
+                    tipo = Tipo.RECEITA
+                ),
+                Transacao(
+                    valor = BigDecimal(20.5),
+                    categoria = "Comida",
+                    tipo = Tipo.DESPESA
+                ),
+                Transacao(
+                    valor = BigDecimal(100),
+                    categoria = "Economia",
+                    tipo = Tipo.RECEITA
+                )
+        )
     }
 }
--- app/src/main/java/br/com/dmagdaleno/financas/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/br/com/dmagdaleno/financas/ui/adapter/ListaTransacoesAdapter.kt
@@ -1,32 +1,65 @@
 package br.com.dmagdaleno.financas.ui.adapter
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import br.com.dmagdaleno.financas.R
 import br.com.dmagdaleno.financas.extension.formatada
+import br.com.dmagdaleno.financas.extension.formatado
+import br.com.dmagdaleno.financas.extension.limitaEm
+import br.com.dmagdaleno.financas.model.Tipo
 import br.com.dmagdaleno.financas.model.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
 
-class ListaTransacoesAdapter(context: Context,
-                             transacoes: List<Transacao>): BaseAdapter() {
+class ListaTransacoesAdapter(val context: Context,
+                             val transacoes: List<Transacao>): BaseAdapter() {
 
-    private val transacoes = transacoes
-    private val context = context
+    private val tamanhoPermitido = 15
 
     override fun getView(posicao: Int, view: View?, parent: ViewGroup?): View {
-        val viewGerada = LayoutInflater
-                .from(context)
+        val viewGerada = LayoutInflater.from(context)
                 .inflate(R.layout.transacao_item, parent, false)
 
         val transacao = transacoes[posicao]
-        viewGerada.transacao_valor.setText("R$ " + transacao.valor.toString())
-        viewGerada.transacao_categoria.setText(transacao.categoria)
-        viewGerada.transacao_data.setText(transacao.data.formatada())
+
+        adicionaIcone(viewGerada, transacao)
+        adicionaValor(viewGerada, transacao)
+        adicionaCategoria(viewGerada, transacao)
+        adicionaData(viewGerada, transacao)
 
         return viewGerada
+    }
+
+    private fun adicionaIcone(view: View, transacao: Transacao) {
+        view.transacao_icone.setBackgroundResource(iconePorTipoDe(transacao))
+    }
+
+    private fun iconePorTipoDe(transacao: Transacao): Int {
+        if(transacao.tipo == Tipo.RECEITA)
+            return R.drawable.icone_transacao_item_receita
+        return R.drawable.icone_transacao_item_despesa
+    }
+
+    private fun adicionaData(view: View, transacao: Transacao) {
+        view.transacao_data.text = transacao.data.formatada()
+    }
+
+    private fun adicionaCategoria(view: View, transacao: Transacao) {
+        view.transacao_categoria.text = transacao.categoria.limitaEm(tamanhoPermitido)
+    }
+
+    private fun adicionaValor(view: View, transacao: Transacao) {
+        view.transacao_valor.text = transacao.valor.formatado()
+        view.transacao_valor.setTextColor(corPorTipoDe(transacao))
+    }
+
+    private fun corPorTipoDe(transacao: Transacao): Int {
+        if(transacao.tipo == Tipo.RECEITA)
+            return ContextCompat.getColor(context, R.color.receita)
+        return ContextCompat.getColor(context, R.color.despesa)
     }
 
     override fun getItem(posicao: Int): Transacao {
