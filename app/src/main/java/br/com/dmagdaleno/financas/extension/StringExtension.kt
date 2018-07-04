62505faebba0bf8605df47e2d48d8f2de07b4c0c
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-03 21:04:25 -0300

    extension function de parse de data

--- app/src/main/java/br/com/dmagdaleno/financas/extension/StringExtension.kt
+++ app/src/main/java/br/com/dmagdaleno/financas/extension/StringExtension.kt
@@ -1,7 +1,16 @@
 package br.com.dmagdaleno.financas.extension
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 fun String.limitaEm(tamanho: Int): String {
     if(this.length > tamanho)
         return "${this.substring(0, tamanho-3)}..."
     return this
+}
+
+fun String.converteEmCalendar(): Calendar {
+    val data = Calendar.getInstance()
+    data.time = SimpleDateFormat("dd/MM/yyyy").parse(this)
+    return data
 }
