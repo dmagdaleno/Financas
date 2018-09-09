203892db8f20c891b9410ea921b4cd6d2f77dbf7
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-09 18:46:25 -0300

    adiciona objetivo a transação

--- app/src/main/java/br/com/dmagdaleno/financas/model/Transacao.kt
+++ app/src/main/java/br/com/dmagdaleno/financas/model/Transacao.kt
@@ -1,9 +1,12 @@
 package br.com.dmagdaleno.financas.model
 
 import java.math.BigDecimal
-import java.util.*
+import java.util.Calendar
 
-class Transacao(val valor: BigDecimal,
-                val categoria: String = "Indefinida",
-                val tipo: Tipo,
-                val data: Calendar = Calendar.getInstance())
+class Transacao(
+        val id: Long? = null,
+        val valor: BigDecimal,
+        val categoria: String = "Indefinida",
+        val tipo: Tipo,
+        val data: Calendar = Calendar.getInstance(),
+        val objetivo: Objetivo? = null)
